7f0c3b8cf641831ee9008285697c242dd7df50b7
shin <142523327+avii1403@users.noreply.github.com>
2023-11-08 22:52:43 +0530

    Add files via upload

---
+++ P1_Caesar.py
@@ -0,0 +1,16 @@
+def encrypt(text,s):
+    result = ""
+    for i in range(len(text)):
+        char = text[i]
+        if (char.isupper()):
+            result += chr((ord(char) + s-65) % 26 + 65)
+        else:
+            result += chr((ord(char) + s - 97) % 26 + 97)
+    return result
+text = input("Enter the word: ")
+s = input("Enter a key: ")
+s = int(s)
+
+print ("Text  : " + text)
+print ("Shift : " + str(s))
+print ("Cipher: " + encrypt(text,s))
